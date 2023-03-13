0074749ef66dcd2d6610240f41ac99612587165d
fdenunez <federicodenunez@gmail.com>
2023-03-13 18:06:35 -0300

    Creé carpetas con el index para cada figura como se tiene que entregar. Voy a hacer una figura mas para tener material para trabajar el miercoles

---
+++ figura3/script_v3.js
@@ -0,0 +1,30 @@
+d3.csv('astronautas.csv', d3.autoType).then(data => { // levantamos el archivo data2000csv
+  console.log(data) // transforma el csv en una lista de objetos. 
+
+  // Guardamos el svg generado en la variable chart
+  let chart3 = Plot.plot({ //plot.plot genera la visualización. Es un SVG 
+    // svg = elemento vectorial y lo guardamos en la variable llamada chart
+    marks: [ // aca elegimos la marca punto para representar cada entidad
+      Plot.dot(data, { // elegimos un punto para representar cada pais
+        x: 'anio_mision', // el canal es la posición en x
+        //y: "cluster", // varable categórica
+        y: "ocupacion",
+        r: "mision_hs", 
+        stroke: "mision_hs",
+        opacity: 0.8,
+      }),
+    ],
+    x: {grid: true,
+      line: true,
+      nice: true,
+      },
+    color: {
+      legend: true,
+      zero: true,
+      nice: true,
+    }
+  })
+
+  // Agregamos chart al div#chart de index.html
+  d3.select('#chart3').append(() => chart3) 
+})
