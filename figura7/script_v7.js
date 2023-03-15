483774c6717434f1f6bf659f96ad7f653ebb21e3
luknda <lucasadnac@hotmail.com>
2023-03-15 09:01:38 -0300

    added figure 7

---
+++ figura7/script_v7.js
@@ -0,0 +1,28 @@
+d3.csv('astronautas.csv', d3.autoType).then(data => {
+  let chart = Plot.plot({
+    marks: [
+      Plot.barY(data, {
+        x: 'status',
+        y: 'mision_hs',
+        sort: "mision_hs",
+        fill: 'eva_mision_hs',
+        title: d => d.nacionalidad + '\n' + d.nombre + "\n" + "Mision Hours: " + d.mision_hs,
+      }),
+    ],
+    y: {
+      line: true,
+      },
+    x: {
+      line: true,
+      nice: true,
+      },
+    color: {
+      legend: true,
+      nice: true,
+    },
+    marginLeft: -0.5,
+    width: 600,
+    height: 800,
+  })
+  d3.select('#chart').append(() => chart)
+})
