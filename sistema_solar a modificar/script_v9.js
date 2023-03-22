09000421bf09552be1b643c8e1f57bc700f640c2
fdenunez <federicodenunez@gmail.com>
2023-03-22 11:39:51 -0300

    Arreglamos el codigo de los planetas

--- sistema_solar a modificar/script_v9.js
+++ sistema_solar a modificar/script_v9.js
@@ -3,51 +3,63 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
 });
 
 function createChart(data) {
+  let rusia = data.filter(d => d.nacionalidad === "U.S.S.R/Rusia")
+  let eeuu = data.filter(d => d.nacionalidad === "EE.UU.")
+  let suma_eeuu = d => sum(d.eva_mision_hs)
+
+  let italia = data.filter(d => d.nacionalidad === "Italia")
+  let francia = data.filter(d => d.nacionalidad === "Francia")
+  let japon = data.filter(d => d.nacionalidad === "Japon")
+  let uk = data.filter(d => d.nacionalidad === "Reino Unido")
   let chart = Plot.plot({
     height: 200,
     line: true,
     zero: true,
     marks: [
-      Plot.dot(data.filter(d => d.nacionalidad === "U.S.S.R/Rusia"), {
+      Plot.dot(rusia, {
         // Jupiter
         //var data = document.querySelector("eva_mision_hs").data;
         x: "nacionalidad",
-        r: "eva_mision_hs",
+        r: d => d.eva_mision_hs,
         fill: "#a59186", //bcafb2 o a59186
-        title: d => "eva_mision_hs: " + d.eva_mision_hs
+        //suma_eeuu
+        title: d => "eva_mision_hs: " + suma_eeuu
       }),
-      Plot.dot(data.filter(d => d.nacionalidad === "EE.UU."), {
+      Plot.dot(eeuu, {
         // Sol
         x: "nacionalidad",
-        r: "eva_mision_hs",
+        r: d => d.eva_mision_hs,
         fill: "#FED811",
+        title: d => "eva_mision_hs: " + suma_eeuu
+      }),
+      Plot.dot(italia, {
+        x: "nacionalidad",
+        r: d => d.eva_mision_hs,
+        fill: "#f1c232",
         title: d => "eva_mision_hs: " + d.eva_mision_hs
       }),
-      Plot.dot(data.filter(d => d.nacionalidad === "Italia"), {
+      Plot.dot(francia, {
         x: "nacionalidad",
-        r: "eva_mision_hs",
-        fill: "#666af0",
+        r: d => d.eva_mision_hs,
+        fill: "#c7c7c7",
         title: d => "eva_mision_hs: " + d.eva_mision_hs
       }),
-      Plot.dot(data.filter(d => d.nacionalidad === "Francia"), {
+      Plot.dot(japon, {
         x: "nacionalidad",
-        r: "eva_mision_hs",
-        fill: "#666af0",
+        r: d => d.eva_mision_hs,
+        fill: "#66cc66",
         title: d => "eva_mision_hs: " + d.eva_mision_hs
       }),
-      Plot.dot(data.filter(d => d.nacionalidad === "Japon"), {
+      Plot.dot(uk, {
         x: "nacionalidad",
-        r: "eva_mision_hs",
-        fill: "#666af0",
-        title: d => "eva_mision_hs: " + d.eva_mision_hs
-      }),
-      Plot.dot(data.filter(d => d.nacionalidad === "Reino Unido"), {
-        x: "nacionalidad",
-        r: "eva_mision_hs",
-        fill: "#666af0",
+        r: d => d.eva_mision_hs,
+        fill: "#e06666",
         title: d => "eva_mision_hs: " + d.eva_mision_hs
       }),
     ],
+    r: {
+      range: [0, 41]
+    },
     marginLeft: -10,
     marginRight: -10,
     symbol: {
@@ -67,13 +79,10 @@ function createChart(data) {
   d3.select("#chart").append(() => chart);
 }
 
-/*const colors = [
-  '#c7c7c7', // Mercury
-  '#f1c232', // Venus
-  '#66cc66', // Earth
-  '#e06666', // Mars
-  '#8ea9e5', // Jupiter
-  '#b4a7d6', // Saturn
-  '#aab0d6', // Uranus
-  '#b0c3d6' // Neptune
+/* Para agregar una dimension mas
+podemos usar los colores o el eje y 
+y filtrar por el porcentaje de militares en los astronautas totales
+para eso hacemos militares / militares + civiles (astronautas)
+y esto define el color del planeta
 ];*/
+// tmb en vez de colores podemos poner una foto el planeta para que sea mas evidente
