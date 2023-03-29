648fa87a9a26d01b0c0cd96789f8492be841189b
fdenunez <federicodenunez@gmail.com>
2023-03-29 16:18:19 -0300

    arreglé finalmente las horas de sistemasolar

--- ..entrega/.sistema_solar.js
+++ ..entrega/.sistema_solar.js
@@ -4,22 +4,22 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
 
 function createChart(data) {
   let rusia = data.filter(d => d.nacionalidad === "U.S.S.R/Rusia")
-  let suma_rusia = d3.sum(rusia, d => d.eva_mision_hs)
+  let suma_rusia = Math.round(d3.sum(rusia, d => d.eva_mision_hs))
 
   let eeuu = data.filter(d => d.nacionalidad === "EE.UU.")
-  let suma_eeuu = d3.sum(eeuu, d => d.eva_mision_hs)
+  let suma_eeuu = Math.round(d3.sum(eeuu, d => d.eva_mision_hs))
 
   let italia = data.filter(d => d.nacionalidad === "Italia")
-  let suma_italia = d3.sum(italia, d => d.eva_mision_hs)
+  let suma_italia = Math.round(d3.sum(italia, d => d.eva_mision_hs))
 
   let francia = data.filter(d => d.nacionalidad === "Francia")
-  let suma_francia = d3.sum(francia, d => d.eva_mision_hs)
+  let suma_francia = Math.round(d3.sum(francia, d => d.eva_mision_hs))
 
   let japon = data.filter(d => d.nacionalidad === "Japon")
-  let suma_japon = d3.sum(japon, d => d.eva_mision_hs)
+  let suma_japon = Math.round(d3.sum(japon, d => d.eva_mision_hs))
 
   let uk = data.filter(d => d.nacionalidad === "Reino Unido")
-  let suma_uk = d3.sum(uk, d => d.eva_mision_hs)
+  let suma_uk = Math.round(d3.sum(uk, d => d.eva_mision_hs))
 
 
   let chart = Plot.plot({
@@ -31,45 +31,44 @@ function createChart(data) {
         // Jupiter
         //var data = document.querySelector("eva_mision_hs").data;
         x: "nacionalidad",
-        r: d => d.eva_mision_hs,
+        r: d => suma_rusia,
         fill: "#a59186", //bcafb2 o a59186
-        //suma_eeuu
-        title: d => "eva_mision_hs: " + suma_rusia
+        title: d => "Horas totales: " + suma_rusia
       }),
       Plot.dot(eeuu, {
         // Sol
         x: "nacionalidad",
-        r: d => d.eva_mision_hs,
+        r: d => suma_eeuu,
         fill: "#FED811",
-        title: d => "eva_mision_hs: " + suma_eeuu
+        title: d => "Horas totales: " + suma_eeuu
       }),
       Plot.dot(italia, {
         x: "nacionalidad",
-        r: d => d.eva_mision_hs,
+        r: d => suma_italia,
         fill: "#f1c232",
-        title: d => "eva_mision_hs: " + suma_italia
+        title: d => "Horas totales: " + suma_italia
       }),
       Plot.dot(francia, {
         x: "nacionalidad",
-        r: d => d.eva_mision_hs,
+        r: d => suma_francia,
         fill: "#c7c7c7",
-        title: d => "eva_mision_hs: " + suma_francia
+        title: d => "Horas totales: " + suma_francia
       }),
       Plot.dot(japon, {
         x: "nacionalidad",
-        r: d => d.eva_mision_hs,
+        r: d => suma_japon,
         fill: "#66cc66",
-        title: d => "eva_mision_hs: " + suma_japon
+        title: d => "Horas totales: " + suma_japon
       }),
       Plot.dot(uk, {
         x: "nacionalidad",
-        r: d => d.eva_mision_hs,
+        r: d => suma_uk,
         fill: "#e06666",
-        title: d => "eva_mision_hs: " + suma_uk
+        title: d => "Horas totales: " + suma_uk
       }),
     ],
     r: {
-      range: [0, 41]
+      range: [0, 45]
     },
     marginLeft: -10,
     marginRight: -10,
