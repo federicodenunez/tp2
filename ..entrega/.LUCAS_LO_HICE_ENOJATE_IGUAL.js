c84cb6b3240bdde5113f448986610b7db204fcd8
fdenunez <federicodenunez@gmail.com>
2023-04-01 18:49:12 -0300

    le puse numeros al grafico astro. Falta aumentar su tamaño y sacar los valores del eje y

--- ..entrega/.LUCAS_LO_HICE_ENOJATE_IGUAL.js
+++ ..entrega/.LUCAS_LO_HICE_ENOJATE_IGUAL.js
@@ -1,44 +1,58 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
-    createChart(data);
+  createChart(data);
+});
+
+function createChart(data) {
+  let yearCounts = d3.rollup(data, 
+    v => v.length, 
+    d => d.anio_mision
+  );
+  
+  let chart = Plot.plot({
+    marks: [
+      Plot.barY(Array.from(yearCounts, ([year, count]) => ({ year, count })), {
+        x: "year",
+        y: "count",
+        reverse: true,
+        width: 0.5,
+        fill: "white" // set bar color to white
+      }),
+      Plot.text(Array.from(yearCounts, ([year, count]) => ({ year, count })), {
+        x: "year",
+        y: d => d.count + 1, // adjust text position to be above bars
+        text: d => d.count,
+        color: "white", // set text color to white
+        align: "center",
+        baseline: "bottom",
+        font: "bold 12 px sans-serif" // adjust font size
+      }),
+    ],
+    x: {
+      line: false, // hide x axis line
+      sort: true,
+      nice: true,
+      label: "", 
+      tickFormat: d3.format("d"),
+      tickValues: d3.range(2010, 2020),
+    },
+    style: {
+      background: "#000124",
+      padding: "3px",
+      color: "white",
+    },
+    y: {
+      label: "Astronautas en misión",
+      domain: [0, d3.max(Array.from(yearCounts, ([year, count]) => count))],
+      noTicks: true // remove ticks from y axis
+    },
   });
   
-  // enojate lo q quieras lo hice igual 
-  function createChart(data) {
-    let yearCounts = d3.rollup(data, 
-      v => v.length, 
-      d => d.anio_mision
-    );
-    
-    let chart = Plot.plot({
-      marks: [
-        Plot.barY(Array.from(yearCounts, ([year, count]) => ({ year, count })), {
-          x: "year", // switched x and y
-          y: "count",
-          reverse: true,
-          width: 0.5, // no cambia nada
-        }),
-      ],
-      x: {
-        line: true,
-        sort: true,
-        nice: true,
-        label: "", 
-        tickFormat: d3.format("d"), // Set the tick label format to remove comma separator
-        tickValues: d3.range(2009, 2019), // Set the tick values to an array of years
-      },
-      style: {
-        background: "#000124",
-        padding: "3px",
-        color: "white",
-      },
-      y: {
-        label: "Astronautas en misión",
-        domain: [0, d3.max(Array.from(yearCounts, ([year, count]) => count))] // changed y domain to use the maximum count
-      },
-      
-    });
-    
-    d3.select("#chart4").append(() => chart);
-  }
+  d3.select("#chart4").append(() => chart);
+}
+
+
+
+
+
   
   
